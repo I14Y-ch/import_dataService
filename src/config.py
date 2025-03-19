0e86e15f0ff01445bd8ed09bf1687aedf018813c
Lien Beroggi <163211986+lberoggi@users.noreply.github.com>
2025-03-19 13:49:57 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,21 +1,21 @@
 
-url = "https://dcat-a.app.cfap02.atlantica.admin.ch/api/Dataservice" #ABNHAME
-# url = "https://dcat.app.cfap02.atlantica.admin.ch/api/Dataservice" #PRODUCTION
+URL = "https://dcat-a.app.cfap02.atlantica.admin.ch/api/Dataservice" #ABNHAME
+# URL = "https://dcat.app.cfap02.atlantica.admin.ch/api/Dataservice" #PRODUCTION
 
 
-token = ""
-id_publisher = ""  # Specify the publisher ID
-swagger_file_path = 'data/swagger.json' #specify the correct file path
+TOKEN = ""
+ID_PUBLISHER = ""  # Specify the publisher ID
+SWAGGER_FILE_PATH = 'data/swagger.json' #specify the correct file path
 # NECESSARY TO UPDATE DATASERVICE
-#id_object = "" #state the id of the object that you need to update
+# ID_OBJECT = "" #state the id of the object that you need to update
 
 
-#if needed
-url_api_root = "" #specify the API url root if endpointUrl is not defined in x-metadata
-language_tag = ""  # Specify the correct language (fr, de, it, en) used in the OpenApi basic specification 
+# IF NEEDED
+URL_API_ROOT = "" #specify the API url root if endpointUrl is not defined in x-metadata
+LANGUAGE_TAG = ""  # Specify the correct language (fr, de, it, en) used in the OpenApi basic specification 
 
-#optional
-url_swagger = "" #specify the Swagger webpage url 
+# OPTIONAL
+URL_SWAGGER = "" #specify the Swagger webpage url 
 
 
 
